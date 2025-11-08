e6f5a801686b8364ae3f09021a9601b50db521bc
tayoxmd <xmd555@gmail.com>
2025-11-08 08:32:56 +0300

    Remove Next.js comparison page and revert to previous state

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,6 @@ import TaskSettings from "./pages/TaskSettings";
 import TaskAccessControl from './pages/TaskAccessControl';
 import TaskArchive from "./pages/TaskArchive";
 import PrivateAccounting from "./pages/PrivateAccounting";
-import NextJsComparison from "./pages/NextJsComparison";
 import PrivateAccountingSettings from "./pages/PrivateAccountingSettings";
 import PrivateCustomers from "./pages/PrivateCustomers";
 import PrivateBookings from "./pages/PrivateBookings";
@@ -129,7 +128,6 @@ const RouterWithTheme = () => {
               <Route path="/email" element={<ProtectedRoute><Email /></ProtectedRoute>} />
               <Route path="/email-settings" element={<ProtectedRoute><EmailSettings /></ProtectedRoute>} />
               <Route path="/role-statistics" element={<ProtectedRoute><RoleStatistics /></ProtectedRoute>} />
-              <Route path="/nextjs-comparison" element={<NextJsComparison />} />
               <Route path="/reset-password" element={<ResetPassword />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
